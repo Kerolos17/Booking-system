dcdcfb67ce6ea9e0c37cdfac5fcbfaff7050809c
Kerolos17 <kerolosmorkos1124@gmail.com>
2025-03-20 18:51:45 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use App\Models\Event;
 use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
-Route::get('/booking', function () {
-    $events = Event::with('bookings')->get();
-    return view('events.index', compact('events'));
-});
 Route::get('/booking', [BookingController::class, 'create']);
 Route::post('/booking', [BookingController::class, 'store'])->name('booking.store');
 Route::get('/booking/confirmation/{booking}', [BookingController::class, 'confirmation'])->name('booking.confirmation');
