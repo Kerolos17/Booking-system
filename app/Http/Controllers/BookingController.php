3c3154e5e6b305da649603a5ad5cbd45348ccbf3
Kerolos Morkos <135118128+Kerolos17@users.noreply.github.com>
2025-03-25 00:30:19 +0200

    qr2

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -49,7 +49,7 @@ class BookingController extends Controller
         ]);
 
         $qrCodeData = $booking->id;
-        $qrCode = QrCode::format('png')->size(200)->generate($qrCodeData);
+        $qrCode = QrCode::format('png')->size(300)->generate($qrCodeData)->margin(10);
 
         $qrPath = 'qrcodes/booking_' . $booking->id . '.png';
         Storage::disk('public')->put($qrPath, $qrCode);
